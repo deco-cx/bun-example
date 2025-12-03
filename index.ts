34fa7ad0c1bea0e6b57139ae6b61835bb1324d93
viktormarinho <viktormpcs@gmail.com>
2025-12-02 21:37:18 -0300

    init

---
+++ index.ts
@@ -0,0 +1,18 @@
+const server = Bun.serve({
+port: 8000,
+fetch(req) {
+    const url = new URL(req.url);
+    
+    if (url.pathname === "/") {
+    return new Response("Hello from Bun on Kubernetes! 🚀");
+    }
+    
+    if (url.pathname === "/_healthcheck") {
+    return new Response("OK", { status: 200 });
+    }
+    
+    return new Response("Not Found", { status: 404 });
+},
+});
+
+console.log(`🚀 Server running on http://localhost:${server.port}`);
